6a6e5197523287d7a1986f07f1d06ce584787268
Brett Wallace <bwallace90@gmail.com>
2015-06-03 13:21:14 -0400

    Add NSUserDefaults to save username and password

--- LoginProject/CreateAccountViewController.swift
+++ LoginProject/CreateAccountViewController.swift
@@ -12,6 +12,9 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var chooseUsernameTextField: UITextField!
     @IBOutlet weak var choosePasswordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    let kUsernameKey = "userNameKey"
+    let kPasswordKey = "passwordKey"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +28,16 @@ class CreateAccountViewController: UIViewController {
     }
     
     @IBAction func createAccountButtonPressed(sender: UIButton) {
+        if choosePasswordTextField.text == confirmPasswordTextField.text && choosePasswordTextField.text.isEmpty != true {
+            NSUserDefaults.standardUserDefaults().setObject(self.chooseUsernameTextField.text, forKey: kUsernameKey)
+            NSUserDefaults.standardUserDefaults().setObject(self.choosePasswordTextField.text, forKey: kPasswordKey)
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
     }
 
     @IBAction func cancelButtonPressed(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
